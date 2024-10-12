4aecc1463c4cb54ff5254d46af1ddacbe4ff5b65
qingdog <69775024+qingdog@users.noreply.github.com>
2024-10-12 09:12:04 +0800

    GitBash环境下更新hosts的github的ip.sh

--- github-ip/Git Bash环境下更新hosts的github的ip（使用前确保 GitHub520 内容在该文件最后部分）.sh
+++
@@ -1,2 +0,0 @@
-#从第一行开始到GitHub520这一行，大写Q不包括当前这一行，重定向输出到hosts文件。即删除文件中GitHub520到最后一行
-sed -i "/# GitHub520 Host Start/Q" c:/Windows/System32/drivers/etc/hosts && curl https://raw.hellogithub.com/hosts >> c:/Windows/System32/drivers/etc/hosts
